8e77177aff9489a630daf02c1d531ecbda129836
mattratto <matt.ratto@utoronto.ca>
2019-10-22 12:37:34 -0400

    Initial commit

---
+++ ArraysConditionals.ino
@@ -0,0 +1,23 @@
+
+int dataVal[] = {2, 7, 4, 6, 5, 3, 9};
+String dataName[] = {"tina","tim","nick","ali","fred","jia","sasha"};   
+int dataCount = 7;   
+
+void setup() {
+  // the array elements are numbered from 0 to (dataCount - 1).
+  // use a for loop to print each item of the array
+  
+  for (int dataNum = 0; dataNum < dataCount; dataNum++) {
+    Serial.println(data[dataNum]);
+  }
+}
+
+void loop() {
+//test your data and print different messages based on this: 
+
+if (data[0]>1) {
+  Serial.println(" 
+}
+
+
+}
